a60143981d289cf97add23c43f883c63680e9302
Aly-leo <aqleonie763nkolo@gmail.com>
2026-06-29 11:44:12 +0100

    feat: setup projet Angular + modele Restaurant + scaffolding des 5 composants

---
+++ src/app/components/restaurant-card/restaurant-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-card',
+  imports: [],
+  templateUrl: './restaurant-card.html',
+  styleUrl: './restaurant-card.scss',
+})
+export class RestaurantCard {}
